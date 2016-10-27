0059a0503258f82118fa6fdef1c40de9ae15ccc9
AHilton <andhilton@gmail.com>
2016-10-27 13:46:09 -0400

    Added a couple of more functions to the Vertex class, and updated the notebook

--- grammar/Vertex.py
+++ grammar/Vertex.py
@@ -16,6 +16,36 @@ class Vertex:
     vec = np.zeros(1,3)
 
 
+    ##
+    # Takes the x,y,z coordinates of the point as arguments
+    #
+    # Default (0,0,0)
+    ##
     def __init__ (self, x, y, z):
 
         self.vec = np.array([x,y,z])
+
+
+    ##
+    # Returns the x-component of the vertex
+    ##
+    def x(self):
+        return self.vec[0]
+
+    ##
+    # Returns the y-component of the vertex
+    ##
+    def y(self):
+        return self.vec[1]
+
+    ##
+    # Returns the z-component of the vertex
+    ##
+    def z(self):
+        return self.vec[2]
+
+    ##
+    # Returns the underlying array, to make adding stuff easier
+    ##
+    def vector(self):
+        return self.vec
