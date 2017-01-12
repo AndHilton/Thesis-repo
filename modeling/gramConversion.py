26c52e86723fe2ddf23af2ba410fdf54246f0d60
AHilton <andhilton@gmail.com>
2017-01-12 13:37:37 -0500

    a lot of files are now deprecated, switching to STLs rather than .blends

--- modeling/gramConversion.py
+++ modeling/gramConversion.py
@@ -11,27 +11,6 @@ import numpy
 
 
 ###
-# takes a Face object in as an argument, and converts its vertex
-# data into a list of tuples that can then be used to create meshes
-###
-def face_toVertList(gramFace):
-    vertList = [tuple(arr.tolist()) for arr in gramFace.getVertices()]
-    return vertList
-
-
-###
-# takes in a GrammarRun object, and adds geometry to the current project
-# and scene to represent the current state of the GrammarRun
-###
-def modelGrammar(gramRun):
-    faceList = gramRun.getFaces()
-    for idx in range(len(faceList)):
-        vertList = face_toVertList(faceList[idx])
-        modeling.add_geometry.add_tetraFace(vertList,"Face")
-    
-
-
-###
 # takes a GrammarRun object, and writes its current configuration
 # to an STL file
 ###
