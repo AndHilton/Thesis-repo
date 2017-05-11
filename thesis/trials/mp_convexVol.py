dd408c4579a3d1a72cddf147d67ec48e4b4dc990
hiltona <hiltona@jupiter1.(none)>
2017-05-11 14:10:50 -0400

    started writing a trial on jupiter, moving it back to my machine

---
+++ thesis/trials/mp_convexVol.py
@@ -0,0 +1,12 @@
+### -----------------------------------------------------------------
+"""
+A trial to optimize for the volume of the convex hull
+"""
+### -----------------------------------------------------------------
+
+import grammar as gram
+import mp_evo
+import modeling
+import os
+
+
