e3bf910252e325e855e7d3215828968875ed35fa
AHilton <andhilton@gmail.com>
2017-05-22 19:26:11 -0400

    added a line to the boilerplate script

--- thesis/trials/run-trial.sh
+++ thesis/trials/run-trial.sh
@@ -1,10 +1,14 @@
 #PBS –N thesis
-#PBS –l procs=10
+#PBS –l procs=12
 #PBS –l walltime=10:00:00
 #PBS –j oe
 
 #module load python/3
 
+
+if [ "$PBS_ENVIRONMENT" != "PBS_INTERACTIVE" ] ; then
+    cd $PBS_O_WORKDIR
+fi
 
 python3 $SCRIPT
 
